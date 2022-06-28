f9bb83ecbbaf93cadaa2d6085d12d7e7999a81d6
kgoa <y-keigoe@ruby-dev.jp>
2022-06-28 16:39:58 +0900

    Fixed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
-  get 'users/index'
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions'
-  }
+  root to: 'books#index'
+  devise_for :users
   resources :users
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
 end
