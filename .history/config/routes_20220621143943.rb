d0584720b9d9c3b95944588ddfee604c5bdbfd57
kgoa <y-keigoe@ruby-dev.jp>
2022-06-29 16:13:06 +0900

    Delete file for .history

--- .history/config/routes_20220621143943.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  get 'users/index'
-  devise_for :users
-  resources :users
-  resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-end
