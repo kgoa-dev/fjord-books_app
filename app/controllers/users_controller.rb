a0923ef26657160480e10d943b24a97fea9308f2
kgoa <y-keigoe@ruby-dev.jp>
2022-07-05 16:38:34 +0900

    Fixed Controller & routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,31 +8,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: t('controllers.common.notice_create', name: User.model_name.human) }
-      else
-        format.html { render :new }
-      end
-    end
-  end
-
-  def update
-    @user = User.find(params[:id])
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: t('controllers.common.notice_update', name: User.model_name.human) }
-      else
-        format.html { render :edit }
-      end
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
 
   root to: 'books#index'
   devise_for :users
-  resources :users
+  resources :users, only: %i[index show]
   resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
