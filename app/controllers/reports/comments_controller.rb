49ac833b527b4db3676f11d0e0413c37682f1269
kgoa <y-keigoe@ruby-dev.jp>
2022-07-05 16:03:46 +0900

    MiniFix

--- app/controllers/reports/comments_controller.rb
+++ app/controllers/reports/comments_controller.rb
@@ -1,15 +1,7 @@
 # frozen_string_literal: true
 
 class Reports::CommentsController < CommentsController
-  before_action :set_commentable, only: %i[create destroy edit update]
-
-  def create; end
-
-  def destroy; end
-
-  def edit; end
-
-  def update; end
+  before_action :set_commentable
 
   private
 
