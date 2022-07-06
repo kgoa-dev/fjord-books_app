b007fbcdb843b1707c25324f1db7b78a44377cdb
kgoa <y-keigoe@ruby-dev.jp>
2022-07-06 10:03:19 +0900

    Fixed follow_relationships_table

--- db/migrate/20220627053712_create_follow_relationships.rb
+++ db/migrate/20220627053712_create_follow_relationships.rb
@@ -1,13 +1,13 @@
 class CreateFollowRelationships < ActiveRecord::Migration[6.1]
   def change
-    create_table :relationships do |t|
+    create_table :follow_relationships do |t|
       t.integer :follower_id
       t.integer :followed_id
 
       t.timestamps null: false
     end
-    add_index :relationships, :follower_id
-    add_index :relationships, :followed_id
-    add_index :relationships, [:follower_id, :followed_id], unique: true
+    add_index :follow_relationships, :follower_id
+    add_index :follow_relationships, :followed_id
+    add_index :follow_relationships, [:follower_id, :followed_id], unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -54,16 +54,9 @@ ActiveRecord::Schema.define(version: 2022_06_27_053712) do
     t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "relationships", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["followed_id"], name: "index_relationships_on_followed_id"
-    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
-    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+    t.index ["followed_id"], name: "index_follow_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_follow_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_follow_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
