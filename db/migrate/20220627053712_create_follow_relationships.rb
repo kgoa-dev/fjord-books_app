819554e67300c2fa7fb4dbae2e2788e4e888cfd5
kgoa <y-keigoe@ruby-dev.jp>
2022-07-06 10:47:19 +0900

    Add 'null: false'

--- db/migrate/20220627053712_create_follow_relationships.rb
+++ db/migrate/20220627053712_create_follow_relationships.rb
@@ -1,8 +1,8 @@
 class CreateFollowRelationships < ActiveRecord::Migration[6.1]
   def change
     create_table :follow_relationships do |t|
-      t.integer :follower_id
-      t.integer :followed_id
+      t.integer :follower_id, null: false
+      t.integer :followed_id, null: false
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -50,8 +50,8 @@ ActiveRecord::Schema.define(version: 2022_06_27_053712) do
   end
 
   create_table "follow_relationships", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["followed_id"], name: "index_follow_relationships_on_followed_id"
